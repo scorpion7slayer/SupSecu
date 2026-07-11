77c6e01cff774d0e5ce5cd1e04884fedb3a8deeb
scorpion7slayer <theodarville@gmail.com>
2026-07-12 01:33:46 +0200

    Prepare SupSécu 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "be.supsecu.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "android.app.Instrumentation"
     }
